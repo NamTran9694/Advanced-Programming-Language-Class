579524ea52adba373cd94355fd0589d1eba46e77
NamTran9694 <92287793+NamTran9694@users.noreply.github.com>
2025-09-18 14:10:47 -0400

    Update python_broken.py

--- Assignment2/Part1/python_broken.py
+++ Assignment2/Part1/python_broken.py
@@ -1,1 +1,11 @@
+# Python: Calculate the sum of an array (BROKEN ON PURPOSE)
+def calculate_sum(arr)   # <- missing colon
+    total = 0
+    for num in arr
+        total += num     # <- missing colon
+    return total
+
+numbers = [1, 2, 3, 4, 5]
+result = calculate_sum(numbers
+print("Sum in Python:", result)
 
