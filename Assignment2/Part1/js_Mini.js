de36015d494687da8aac0f3fb6bbd400c2386a16
NamTran9694 <92287793+NamTran9694@users.noreply.github.com>
2025-09-19 09:39:44 -0400

    Update js_Mini.js

--- Assignment2/Part1/js_Mini.js
+++ Assignment2/Part1/js_Mini.js
@@ -15,8 +15,8 @@ for (let j = 0; j < 3; j++) good.push(() => j);
 
 // Demo
 const add7 = makeAdder(7);
-console.log(add5(10));   // [17, 1]
-console.log(add5(2));    // [9, 2]
+console.log(add7(10));   // [17, 1]
+console.log(add7(2));    // [9, 2]
 console.log(bad.map(f => f()));   // [3, 3, 3]  (pitfall)
 console.log(good.map(f => f()));  // [0, 1, 2]  (correct)
 
